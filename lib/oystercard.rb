45a749c26cee5e2bf01e1311aa01835cd3293032
diok22 <Dionysis.k22@gmail.com>
2017-02-28 15:02:03 +0000

    creates top up method with maximum value constant

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,16 @@
 class Oystercard
 
+  MAXIMUM_BALANCE = 90
+
   attr_reader :balance
 
   def initialize
     @balance = 0
   end
 
+  def top_up(money)
+    raise 'Top up balance cannot exceed 90 pounds' if @balance + money > MAXIMUM_BALANCE
+    @balance += money
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,19 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+      expect{ subject.top_up(5) }.to change{ subject.balance }.by(5)
+    end
+
+    it 'raises an error if the maximum balance is exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect{ subject.top_up 1 }.to raise_error 'Top up balance cannot exceed 90 pounds'
+    end
+
+  end
+
 end
